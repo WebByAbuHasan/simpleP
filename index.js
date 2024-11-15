cf93ab762e062e4ba361df6711a2795040e5ae1b
Abu Hasan <abuhasan43098@gmail.com>
2024-11-15 14:52:33 +0600

    add backend (nord js) file index js

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+console.log("hello im a sample web project creator");
+
+var a=10;
+var b=20;
+var c=30;
+var d=a+b+c;
+
+console.log(d);
+
+var aa=" hello world";
+var bb= null;
+var cc ;
+
+console.log(aa);
+console.log(bb);
+console.log(cc);
+console.log(aa);
