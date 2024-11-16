8c333af368ffd1061a5c20e04b8af61bc554abf2
Abu Hasan <abuhasan43098@gmail.com>
2024-11-16 16:23:18 +0600

    array if eles switch lopp added

--- index.js
+++ index.js
@@ -1,21 +1,97 @@
 console.log("hello world");
-console.log("hello im a sample web project creator");
+console.log("hello, I'm a sample web project creator");
 
-let a=10;
-let b=20;
-let c=30;
-let d=a+b+c;
-
+// This is data type
+let a = 10, b = 20, c = 30, d = a + b + c;
 console.log(d);
 
-let aa=" hello world";
-let bb= null;
-let cc;
+let aa = "Hello world";
+let bb = null;
+// let cc;
 
 console.log(aa);
 console.log(bb);
-console.log(cc);
-console.log(aa);
+// console.log(cc);
 
-const arr = ("abu", "aka", "dish");
-console.log(arr);
+// This is array
+const arr = ["abu", "asik", "disha", "sompa", "tompa"];
+console.log("Array contents:", arr);
+
+// This is an object
+const car = {
+    type: "Fiat",
+    model: "500",
+    color: "white"
+};
+console.log("Car object:", car);
+
+// This is an if and else statement
+let age = 40;
+let city = "raj";
+
+if (age > 18 && city === "raj") {
+    console.log("This person is an adult");
+} else {
+    console.log("This person is not an adult");
+}
+
+/* if else if statement
+let marks = 32;
+if (marks>=80){
+    document.write("A+");
+}
+else if (marks<79 && marks>=70){
+    document.write("A");
+}
+else if (marks<69 && marks>=60){
+    document.write("A-");
+}
+else if (marks<59 && marks>=50){
+    document.write("B+");
+}
+else if (marks<49 && marks>=40){
+    document.write("B");
+}
+else if (marks<39 && marks>=33){
+    document.write("C");
+}
+else {
+    document.write("F");
+}
+switch case
+let mark=45;
+switch (true){
+        case (mark<100 && mark>=80):
+            document.write("A+");
+        break;
+        case (mark<79 && mark>=70):
+            document.write("A");
+        break;
+        case (mark<69 && mark>=60):
+            document.write("A-");
+        break;
+        case (mark<59 && mark>=50):
+            document.write("B");
+        break;
+        case (mark<49 && mark>=40):
+            document.write("B-");
+        break;
+        case (mark<39 && mark>=33):
+            document.write("C");
+        break;
+        default:
+            document.write("F");
+        break;
+ }*/
+//  loop
+let i ;
+for (i = 0; i < 100; i++) {
+    console.log(i);
+}
+
+
+
+
+
+
+
